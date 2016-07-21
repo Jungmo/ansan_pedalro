f95297e13aee018e5039dbd57e5979a1c2d07d65
Jungmo <ajm100@ajou.ac.kr>
2016-07-22 02:00:42 +0900

    test csv file and modify code

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import requests
-from bs4 import BeautifulSoup
 import time
 import string
 import urllib2
@@ -17,9 +15,11 @@ url='http://www.pedalro.kr/station/station.do?method=stationState&menuIdx=st_01'
 ts = time.strftime("%Y-%m-%d-%I:%M", time.localtime())
 filename = str(ts) + ".csv"
 f = open(filename, "w")
+count = 1
 
-
-while(1) 
+while True :
+	print("%d번째 저장입니다.")%(count) 
+	ts = time.strftime("%Y-%m-%d-%I:%M", time.localtime())
 	for line in urllib2.urlopen(url):
 		if "			title : " in line :
 			s = line.lstrip()
@@ -27,10 +27,10 @@ while(1)
 			s = s.rstrip("\n")
 			s = s.split("|")
 			dict[s[0]] = s[3]
-
-		for i in dict.keys():
+	for i in dict.keys():
 		f.write(ts + ",")
 		f.write(str(i) +","+ str(dict[i]))
 		f.write("\n")
 	
+	dict={}
 	time.sleep(args.interval)
