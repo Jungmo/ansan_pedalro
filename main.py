aa9736511d0cfef39582960924ff4a72170ffd8a
Jungmo <ajm100@ajou.ac.kr>
2016-07-22 01:12:52 +0900

    add time interval

--- main.py
+++ main.py
@@ -4,23 +4,33 @@ from bs4 import BeautifulSoup
 import time
 import string
 import urllib2
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--interval', type = int, default='300', help='seconds')
+args = parser.parse_args()
 
 dict = {}
 url='http://www.pedalro.kr/station/station.do?method=stationState&menuIdx=st_01'# + str(page)
-
-for line in urllib2.urlopen(url):
-	if "			title : " in line :
-		s = line.lstrip()
-		s = s.lstrip("title : '")
-		s = s.rstrip("\n")
-		s = s.split("|")
-		dict[s[0]] = s[3]
 
 ts = time.strftime("%Y-%m-%d-%I:%M", time.localtime())
 filename = str(ts) + ".csv"
 f = open(filename, "w")
 
-for i in dict.keys():
-	f.write(str(i) +","+ str(dict[i]))
-	f.write("\n")
 
+while(1) 
+	for line in urllib2.urlopen(url):
+		if "			title : " in line :
+			s = line.lstrip()
+			s = s.lstrip("title : '")
+			s = s.rstrip("\n")
+			s = s.split("|")
+			dict[s[0]] = s[3]
+
+		for i in dict.keys():
+		f.write(ts + ",")
+		f.write(str(i) +","+ str(dict[i]))
+		f.write("\n")
+	
+	time.sleep(args.interval)
