1dbac20d884fecf6f183f8063c1c649467a9d32b
Jungmo <ajm100@ajou.ac.kr>
2016-07-22 19:01:39 +0900

    Solve : Issue #1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import string
 import urllib2
 import argparse
-
+import sys
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--interval', type=int, default='300', help='seconds')
@@ -16,10 +16,11 @@ ts = time.strftime("%Y-%m-%d-%I-%M", time.localtime())
 filename = str(ts) + ".csv"
 f = open(filename, "w")
 count = 1
-
+f.write("ts,")
 while True:
     print("%d번째 저장입니다.") % (count)
-    ts = time.strftime("%Y-%m-%d-%I:%M", time.localtime())
+    ts = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+
     for line in urllib2.urlopen(url):
         if "			title : " in line:
             s = line.lstrip()
@@ -27,11 +28,22 @@ while True:
             s = s.rstrip("\n")
             s = s.split("|")
             dict[s[0]] = s[3]
-    for i in dict.keys():
-        f.write(ts + ",")
-        f.write(str(i) + "," + str(dict[i]))
+    if count == 1:
+        for i in dict.keys():
+            f.write(str(i) + ",")
         f.write("\n")
+        f.write(ts + ",")
+        for i in dict.keys():
+            f.write(str(dict[i]) + ",")
+        f.write("\n")
+
+    else:
+        f.write(ts + ",")
+        for i in dict.keys():
+             f.write(str(dict[i]) + ",")
+        f.write("\n")
+
     count += 1
-    dict = {}
+    dict.clear()
     time.sleep(args.interval)
 
